e57e546ab92049fc504cffe1cc1dcab0604b7035
David Nichols <mybluevan@gmail.com>
2015-10-23 16:52:15 -0500

    Tutorial, step 7.

--- closet.py
+++ closet.py
@@ -52,5 +52,43 @@ def close_db(error):
     if db is not None:
         db.close()
 
+@app.route('/')
+def show_garments():
+    db = get_db()    
+    cur = db.execute('select description from garments order by id desc')
+    garments = [dict(description=row[0]) for row in cur.fetchall()]
+    return render_template('show_garments.html', garments=garments)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into garments (description) values (?)',
+                 [request.form['description'],])
+    db.commit()
+    flash('New garment was successfully added')
+    return redirect(url_for('show_garments'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_garments'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_garments'))
+
 if __name__ == '__main__':
     app.run()
