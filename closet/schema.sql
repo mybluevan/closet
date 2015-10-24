f3eb23ff90d5e0f96fa4886ae9755ef65e3b4418
David Nichols <mybluevan@gmail.com>
2015-10-24 00:20:27 -0500

    New schema

--- closet/schema.sql
+++ closet/schema.sql
@@ -1,5 +1,51 @@
+PRAGMA foreign_keys = ON;
+drop table if exists categories;
+create table categories (
+  slug text primary key not null,
+  parent integer references categories,
+  name text not null,
+  picture text
+);
+create index parentindex on categories(parent);
+drop table if exists colours;
+create table colours (
+  id integer primary key autoincrement,
+  name text not null,
+  code text not null
+);
+drop table if exists brands;
+create table brands (
+  id integer primary key autoincrement,
+  name text not null,
+  logo text
+);
+drop table if exists styles;
+create table styles (
+  id integer primary key autoincrement,
+  name text not null,
+  picture text
+);
+drop table if exists attributes;
+create table attributes (
+  id integer primary key autoincrement,
+  name text not null
+);
 drop table if exists garments;
 create table garments (
   id integer primary key autoincrement,
-  description text not null
+  category text references categories not null,
+  brand integer references brands,
+  style integer references styles,
+  primarycolour integer references colours,
+  secondarycolour integer references colours,
+  description text,
+  picture text
+);
+create index garmentindex on garments(category, brand, style, primarycolour);
+create index secondarycolourindex on garments(secondarycolour) where secondarycolour is not null;
+drop table if exists garment_attributes;
+create table garment_attributes (
+  garment integer references garments not null,
+  attr integer references attributes not null,
+  primary key (garment, attr)
 );
